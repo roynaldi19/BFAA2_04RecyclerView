c1a04dec291b93a372166a41ca421bb8f20dbe85
roynaldi19 <roynaldi19@gmail.com>
2021-02-19 19:32:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bfaa2_04recyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package com.example.bfaa2_04recyclerview
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
